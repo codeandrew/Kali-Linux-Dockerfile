5db7730ef3551224c2109324f1b941a33a9efd1d
Jean Andrew Fuentes <jeanandrewfuentes@gmail.com>
2023-04-19 17:52:11 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 NAME=kali-container
 IMAGE=codeandrew/kali-rolling:latest
+KALI_DIR=$HOME/kali
+mkdir -p $KALI_DIR
 set -x
 
 RUNNING_INSTANCE=`docker ps | grep $NAME`
@@ -8,7 +10,7 @@ if [ -z "$RUNNING_INSTANCE" ]
 then
     echo "No Running Instance of $NAME"
     docker rm $NAME
-    docker run -it --tty -v scripts:/tmp -v /var/run.docker.sock:/var/run/docker.sock -p 9990-9999:9990-9999 --name $NAME $IMAGE 
+    docker run -it --tty -v $KALI_DIR:/home -v /var/run.docker.sock:/var/run/docker.sock -p 4440-4449:4440-4449 --name $NAME $IMAGE 
 else
     echo "Entering Existing Container"
     docker exec -it $NAME bash
